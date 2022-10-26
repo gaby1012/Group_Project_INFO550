f0446663cdd7a9c35b8c210801f92f823144fd31
gblade <gblade@emory.edu>
2022-10-26 16:39:02 -0400

    made small updates to filepaths and moved file to correct folder

---
+++ code/hs_code/02_make_figure.R
@@ -0,0 +1,26 @@
+here::i_am(
+  "code/hs_code/02_make_figure.R"
+)
+library(ggplot2)
+
+
+df <- readRDS(
+  file = here::here("data/data_clean.rds")
+)
+
+agg_tbl<-df %>%
+  group_by(topic) %>% dplyr::summarize(sum_time = sum(length))
+
+
+fig<- ggplot(agg_tbl, aes(x=factor(topic), weight=sum_time,fill=topic))+
+  geom_bar()+
+  ggtitle("Topic vs. Time Length (min)") +
+  xlab("Topic") + ylab("Time Length (min)")+theme(plot.title = element_text(hjust = 0.5))
+
+
+
+ggsave(
+  here::here("output/figure.png"),
+  plot = fig,
+  device = "png"
+)
